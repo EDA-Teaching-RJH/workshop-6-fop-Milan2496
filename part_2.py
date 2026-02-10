c530d5bac9a71365b758cd7fc11a70220c127080
Milan2496 <mg101@kent.ac.uk>
2026-02-10 13:13:34 +0000

    task 2.3

--- part_2.py
+++ part_2.py
@@ -11,3 +11,27 @@ print(rover_status["Battery"])
 (rover_status["Speed"]) = 5
 
 print(rover_status)
+
+mission_log = [
+    {   "Dictionary1" : {
+            "Site": "Crater A",
+            "Radiation": "Low",
+            "Water": False,
+        }
+    },
+
+    {
+        "Dictionary2" : {
+            "Site": "Dune B",
+            "Radiation": "High",
+            "Water": True,
+        }
+    }
+]
+num = 0
+for i in range(len(mission_log)):
+    num = num + 1
+    if num == 1:
+        print(f"Site {mission_log[i]["Dictionary1"]["Site"]} has {mission_log[i]["Dictionary1"]["Radiation"]} radiation.")
+    elif num == 2:
+        print(f"Site {mission_log[i]["Dictionary2"]["Site"]} has {mission_log[i]["Dictionary2"]["Radiation"]} radiation.")
