d1d3cc8cc4d81b9793ef463c3611af9fbe625db8
Milan2496 <mg101@kent.ac.uk>
2026-02-10 12:17:43 +0000

    task 1.1

--- part_1.py
+++ part_1.py
@@ -1,1 +1,7 @@
+sample_bay = ["Basalt", "Silica", "Iron", "Dust"]
 
+print(f"first item = {sample_bay[0]}")
+i = int(len(sample_bay)) - 1
+print(f"last item = {sample_bay[i]}")
+j = len(sample_bay)
+print(f"total number of samples in bay = {j}")
