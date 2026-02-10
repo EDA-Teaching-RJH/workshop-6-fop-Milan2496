879b24ac25b29a6056bc1d891814cadd5f457194
Milan2496 <mg101@kent.ac.uk>
2026-02-10 12:29:37 +0000

    task 1.3

--- part_1.py
+++ part_1.py
@@ -1,4 +1,6 @@
 sample_bay = ["Basalt", "Silica", "Iron", "Dust"]
+new_findings = []
+
 
 print(f"first item = {sample_bay[0]}")
 i = int(len(sample_bay)) - 1
@@ -8,3 +10,14 @@ print(f"total number of samples in bay = {j}")
 
 for sample_name in range(len(sample_bay)):
     print(f"Transmitting data for: {sample_bay[sample_name]}")
+
+count = 3
+while count > 0 :
+    rock_name = input("Enter the name of a rock: ")
+    count = count - 1 
+    new_findings.append(rock_name)
+
+print("New Findings:")
+
+for i in range(len(new_findings)):   
+    print(f"{new_findings[i]}")
